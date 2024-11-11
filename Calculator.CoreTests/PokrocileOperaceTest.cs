af7229210967144a7b71cd18296cb135ab4f0329
MrVendys <72186908+MrVendys@users.noreply.github.com>
2024-11-11 14:09:07 +0100

    CHANGE - Vytvoření projektu na testy: Vytvoření testů na základní výpočty se všemi operacemi.

---
+++ Calculator.CoreTests/PokrocileOperaceTest.cs
@@ -0,0 +1,68 @@
+﻿using Calculator.Core.Strategies;
+
+namespace Calculator.CoreTests
+{
+    [TestClass]
+    public class PokrocileOperaceTest
+    {
+
+        [TestMethod]
+        [DataRow(2, 3, 8)]
+        [DataRow(5, 0, 1)]
+        [DataRow(-2, 3, -8)]
+        [DataRow(4, 0.5, 2)]
+        [DataRow(3, -2, 0.1111)]
+        public void Power(double cislo1, double cislo2, double ocekavanyVysledek)
+        {
+            OperationStrategyBase op = new PowerStrategy();
+
+            double skutecnyVysledek = op.Vypocitej(cislo1, cislo2);
+
+            Assert.AreEqual(ocekavanyVysledek, skutecnyVysledek);
+        }
+
+        [TestMethod]
+        [DataRow(9, 3)]
+        [DataRow(0, 0)]
+        [DataRow(2.25, 1.5)]
+        [DataRow(0.25, 0.5)]
+        [DataRow(10, 3.1623)]
+        public void SquareRoot(double cislo1, double cislo2, double ocekavanyVysledek)
+        {
+            OperationStrategyBase op = new SquareRootStrategy();
+
+            double skutecnyVysledek = op.Vypocitej(cislo1, cislo2);
+
+            Assert.AreEqual(ocekavanyVysledek, skutecnyVysledek);
+        }
+
+        [TestMethod]
+        [DataRow(0, 1)]
+        [DataRow(1, 1)]
+        [DataRow(5, 120)]
+        [DataRow(7, 5040)]
+        [DataRow(3, 6)]
+        public void Factorial(double cislo1, double ocekavanyVysledek)
+        {
+            OperationStrategyBase op = new FactorialStrategy();
+
+            double skutecnyVysledek = op.Vypocitej(cislo1);
+
+            Assert.AreEqual(ocekavanyVysledek, skutecnyVysledek);
+        }
+
+        [TestMethod]
+        [DataRow(10, 3, 1)]
+        [DataRow(9, -4, 1)]
+        [DataRow(-7, 3, -1)]
+        [DataRow(15, 5, 0)]
+        public void Modulo(double cislo1, double cislo2, double ocekavanyVysledek)
+        {
+            OperationStrategyBase op = new ModuloStrategy();
+
+            double skutecnyVysledek = op.Vypocitej(cislo1, cislo2);
+
+            Assert.AreEqual(ocekavanyVysledek, skutecnyVysledek);
+        }
+    }
+}
---
+++ Calculator.CoreTests/ZakladniOperaceTest.cs
@@ -0,0 +1,69 @@
+﻿using Calculator.Core.Strategies;
+
+namespace Calculator.CoreTests
+{
+    [TestClass]
+    public class ZakladniOperaceTest
+    {
+
+        [TestMethod]
+        [DataRow(5, 3, 8)]
+        [DataRow(-2, 4, 2)]
+        [DataRow(0, 7, 7)]
+        [DataRow(2.5, 3.2, 5.7)]
+        [DataRow(-1.5, -3.5, -5)]
+        public void Scitani(double cislo1, double cislo2, double ocekavanyVysledek)
+        {
+            OperationStrategyBase op = new PlusStrategy();
+
+            double skutecnyVysledek = op.Vypocitej(cislo1, cislo2);
+
+            Assert.AreEqual(ocekavanyVysledek, skutecnyVysledek);
+        }
+
+        [TestMethod]
+        [DataRow(10, 5, 5)]
+        [DataRow(3, -4, 7)]
+        [DataRow(0, 8, -8)]
+        [DataRow(7.5, 2.3, 5.2)]
+        [DataRow(-6, -2, -4)]
+        public void Odcitani(double cislo1, double cislo2, double ocekavanyVysledek)
+        {
+            OperationStrategyBase op = new MinusStrategy();
+
+            double skutecnyVysledek = op.Vypocitej(cislo1, cislo2);
+
+            Assert.AreEqual(ocekavanyVysledek, skutecnyVysledek);
+        }
+
+        [TestMethod]
+        [DataRow(4, 3, 12)]
+        [DataRow(-3, 5, -15)]
+        [DataRow(0, 6, 0)]
+        [DataRow(1.5, 2.5, 3.75)]
+        [DataRow(-2, -4, 8)]
+        public void Nasobeni(double cislo1, double cislo2, double ocekavanyVysledek)
+        {
+            OperationStrategyBase op = new MultiplyStrategy();
+
+            double skutecnyVysledek = op.Vypocitej(cislo1, cislo2);
+
+            Assert.AreEqual(ocekavanyVysledek, skutecnyVysledek);
+        }
+
+        [TestMethod]
+        [DataRow(10, 2, 5)]
+        [DataRow(-9, 3, -3)]
+        [DataRow(0, 5, 0)]
+        [DataRow(7.5, 2.5, 3)]
+        [DataRow(-6, -2, 3)]
+        public void Deleni(double cislo1, double cislo2, double ocekavanyVysledek)
+        {
+            OperationStrategyBase op = new DivideStrategy();
+
+            double skutecnyVysledek = op.Vypocitej(cislo1, cislo2);
+
+            Assert.AreEqual(ocekavanyVysledek, skutecnyVysledek);
+        }
+    }
+}
